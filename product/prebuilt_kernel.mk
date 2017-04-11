7536ad59028b2ebd839623ab770e27939a4a6036
Mohd Faraz <Checkingprop@gmail.com>
2017-04-12 01:01:49 +0530

    Update prebuilt_kernel.mk

--- product/prebuilt_kernel.mk
+++ product/prebuilt_kernel.mk
@@ -1,6 +1,6 @@
 # Prebuilt Kernel
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := $(LOCAL_PATH)/prebuilts/kernel
+	LOCAL_KERNEL := $(LOCAL_PATH)/G42-kernel/kernel
 else
 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
