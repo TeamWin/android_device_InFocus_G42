4c323a28d97ad4f4dae85075669785ec00be72ef
Mohd Faraz <Checkingprop@gmail.com>
2017-04-14 01:09:39 +0530

    Error fixed

--- lineage.mk
+++ lineage.mk
@@ -18,10 +18,10 @@
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/InFocus/G42/device_G42.mk)
@@ -34,7 +34,7 @@ EXTENDED_FONT_FOOTPRINT := true
 PRODUCT_DEVICE := G42
 PRODUCT_NAME := Lineage_G42
 PRODUCT_BRAND := InFocus
-PRODUCT_MODEL := M680_00WW
+PRODUCT_MODEL := InFocus M680
 PRODUCT_MANUFACTURER := InFocus
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
