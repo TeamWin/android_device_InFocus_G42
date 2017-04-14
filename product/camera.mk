419c3aac75628d2087f061d459e95f4f0b297a70
Mohd Faraz <Checkingprop@gmail.com>
2017-04-14 14:53:35 +0530

    Fixed

--- product/camera.mk
+++ product/camera.mk
@@ -1,9 +1,14 @@
 # Camera
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
-    $(LOCAL_PATH)/configs/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    $(LOCAL_PATH)/camera/camerasize.xml:system/etc/camerasize.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.camera.raw.xml:system/etc/permissions/android.hardware.camera.raw.xml
 
 PRODUCT_PACKAGES += \
-    Snap
+    Snap \
+    libcamera_parameters_ext
+
+# Camera Legacy
+PRODUCT_PACKAGES += \
+    fs_config_files
